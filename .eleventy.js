c4802078b479c76a8a3034b856523b2cbdfbf93e
lucmaki <thelucluigi@gmail.com>
2023-08-03 00:58:40 +0200

    add liquid config

--- .eleventy.js
+++ .eleventy.js
@@ -4,12 +4,18 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("graphs");
   eleventyConfig.addPassthroughCopy("imgs");
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("_includes");
+
   eleventyConfig.setLibrary("md", markdownIt({
     html: true,
     linkify: true,
     typographer: true
   }).use(markdownItAnchor));
+  
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: true,
+    strict_filters: true
+  });
+
   return {
     dir: {
       input: ".",
