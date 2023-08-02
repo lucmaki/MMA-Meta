a3067092640c5b8cfa6e97a27f118b006155f6d6
lucmaki <thelucluigi@gmail.com>
2023-08-03 01:01:26 +0200

    copy includes graphs

--- .eleventy.js
+++ .eleventy.js
@@ -4,18 +4,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("graphs");
   eleventyConfig.addPassthroughCopy("imgs");
   eleventyConfig.addPassthroughCopy("css");
-
   eleventyConfig.setLibrary("md", markdownIt({
     html: true,
     linkify: true,
     typographer: true
   }).use(markdownItAnchor));
-  
-  eleventyConfig.setLiquidOptions({
-    dynamicPartials: true,
-    strict_filters: true
-  });
-
   return {
     dir: {
       input: ".",
